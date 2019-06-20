c49e7b7e233b85f6d613254f4d1c2355e2420f4e
Ahmed ElSenousi <asenousy@gmail.com>
2019-06-20 17:16:45 +0100

    Updates

---
+++ precache-manifest.e0f4e85f1c8c63380a73080944446bd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "377135af4f101e51039b",
+    "url": "/ideas-board/static/js/runtime~main.3b0d2449.js"
+  },
+  {
+    "revision": "73e31ba8e7a1da6e7d72",
+    "url": "/ideas-board/static/js/main.cfa45fe9.chunk.js"
+  },
+  {
+    "revision": "e1dedf5e492ccae30a14",
+    "url": "/ideas-board/static/js/2.c703f512.chunk.js"
+  },
+  {
+    "revision": "73e31ba8e7a1da6e7d72",
+    "url": "/ideas-board/static/css/main.50d98e01.chunk.css"
+  },
+  {
+    "revision": "572906dc7f7a14f96633ff1dc176f4a2",
+    "url": "/ideas-board/index.html"
+  }
+];
